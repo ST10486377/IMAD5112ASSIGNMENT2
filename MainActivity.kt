65ccd466d1d1b6f848fc7b04cea003eee1e2ddb9
ST10486377 <st10486377@rcconnect.edu.za>
2025-05-16 20:53:15 +0200

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,22 @@
+welcome
+
+// Welcome Screen
+package com.example.assignment2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            val intent = Intent(this, QuestionActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
